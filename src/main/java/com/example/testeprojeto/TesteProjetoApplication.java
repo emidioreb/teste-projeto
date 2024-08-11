6b7f2afbcbb1b6088350aa0636391c46a688b1fc
emidioreb <emidioreboucas96@gmail.com>
2024-08-11 19:20:40 -0300

    first commit

---
+++ src/main/java/com/example/testeprojeto/TesteProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteProjetoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testeprojeto/TesteProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteProjetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
